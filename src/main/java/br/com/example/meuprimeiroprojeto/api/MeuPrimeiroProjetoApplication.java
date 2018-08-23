969e31b955ec56c243d21f058200004bb874f516
Ezequiel Messore <ezequiel.messore@s2it.com.br>
2018-08-23 12:07:34 -0300

    Adding project

---
+++ src/main/java/br/com/example/meuprimeiroprojeto/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,12 @@
+package br.com.example.meuprimeiroprojeto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+}
